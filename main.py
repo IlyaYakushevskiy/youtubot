f4827774980e6cd46095e73a5819afb6c27fd3dc
Ilya Y <ilyayakushevskiy3@gmail.com>
2023-02-12 16:20:57 +0100

    nuzhna dodelat

--- main.py
+++ main.py
@@ -16,10 +16,7 @@ print("Input files found..")
 #title_with_codec = movie_render_AI.get_title_format_string()[:-1] + ".mp4"
 topic = input("input topic: Am I the Asshole for ")
 
-text = "Am I the Asshole for for beeing so talanted and good in whatever I do. Post: So I'm a 20 year old guy and I've had a lot of luck with pretty much whatever I've set my mind to. I'm in university studying computer science, I'm really good at sports, I make some money with coding projects and whatever else I try my hand at seems to turn out pretty successful. :The problem is twofold. On one hand, my friends and family tend to get really jealous of me and resent my success which is really hurtful. On the other hand, I find myself constantly comparing myself to others and worrying that I'm not good enough and that I should be doing better, much better. I'm kinda in a state of paralysis and feeling like I can only go so far and then it all falls apart:Am I the Asshole for having too much natural talent?"
-
-"""USE FOR PRODUCTION ONLY"""
-#text = "Am I the Asshole for " + topic + transcript.get_transript(topic)
+text = "Am I the Asshole for Breaking  Up with Him Over His  Hot Dog Habit? Post: I met this amazing guy a few  months ago. We hit it  off right away and had  an amazing  first couple of  dates. We started seeing  each other more often,  and things were really good , until I noticed something strange . :Every time I went to  his house, he was eating a hot  dog. Just one hot dog. Every  single time. I couldn't believe it . :At first I tried not to be too judgmental , but it got to a point where I  was getting really annoyed. I 'd try to talk to him  while he was eating,  and he'd just be picking at  his hot dog the whole time . :I was getting frustrated, so  I eventually talked to him about it . He just kind of laughed and  said it was no big deal.  So I broke up with him. :Now  friends are telling me I'm being an asshole. Was I out of line for ending our relationship just because he ate a hot dog every day?"
 
 separator = "Post:"
 if separator in text:
@@ -31,6 +28,18 @@ else:
 print("title name is :", title_name)
 print("full text:", text)
 
+words = rest_of_text.split()
+new_text = []
+for i in range(0, len(words), 4):
+    new_text.append(" ".join(words[i:i+4]))
+new_text = "\n".join(new_text)
+text = title_name + new_text
+
+"""USE FOR PRODUCTION ONLY"""
+#text = "Am I the Asshole for " + topic + transcript.get_transript(topic)
+
+
+
 #title_name = text[:text.find("?")+1] #Creates title name (Ex: Am I the Asshole for giving sloppy toppy to my hairdresser?)
 
 transcript.filewriter("redditAIDAscript.txt", text) #should rename to "temp" or smth
@@ -39,13 +48,19 @@ transcript.filewriter("redditAIDAscript.txt", text) #should rename to "temp" or 
 tts_record(text)
 make_srt(text)
 
-logging.basicConfig(filename="log.txt", level=logging.INFO, format='%(asctime)s:%(message)s') # here we do beautul log 
+logging.basicConfig(filename="log.txt", level=logging.INFO, format='%(asctime)s:%(message)s') # here we do  log 
 logging.info(text)
 
 
-#print(title_with_codec)
+
 movie_render_AI.video_construct(img_source, video_source, audio_source, subtitle_source, title_name)
-#yt_upload.video_uploader(vid_name, )
+
+
+
+
+
+#print(title_with_codec)
+# #yt_upload.video_uploader(vid_name, )
 #movie_render_AI.video_split("D:/thrash/vid_cut.mp4")
 #vid_upload()
 
--- srt_gen.py
+++ srt_gen.py
@@ -20,36 +20,32 @@ def split_text_into_subtitles(text, max_length):
     return chunks
 
 def make_srt(text):
-    # Split the text into chunks
-    chunks = split_text_into_subtitles(text, 26)   
-    
-    tts_speed = (round(audio.info.length)+1)/len(text) # time pro 1 char tts 
-    # Create a list of subtitles
-    subtitles = []
-    chr_prev = 0
-    for i, chunk, in enumerate(chunks):
-        # Create a new SubRipItem
-        sub = pysrt.SubRipItem()
-        sub.index = i + 1
-        sub.text = chunks[i-1] + "\n" + chunks[i]
-        sub.start.seconds = chr_prev * tts_speed 
-        sub.end.seconds = (chr_prev + len(sub.text)) * tts_speed 
-        chr_prev += len(sub.text)
-        # Append the SubRipItem to the list of subtitles
-        subtitles.append(sub)
-
-    # Create a SubRipFile object
-    subs = pysrt.SubRipFile(subtitles)
-
-    # Save the subtitles to an SRT file
-    subs.save("./sources/subtitles.srt", encoding='utf-8')
+     # Split the text into chunks
+     chunks = text.split(".")
+     tts_speed = audio.info.length/len(text) # time pro 1 char tts 
+     # Create a list of subtitles
+     subtitles = []
+     chr_prev = 0
+     for i, chunk, in enumerate(chunks):
+         # Create a new SubRipItem
+         sub = pysrt.SubRipItem()
+         sub.index = i + 1
+         sub.text = chunk
+         sub.start.seconds = chr_prev * tts_speed * 100
+         sub.end.seconds = (chr_prev + len(chunk[i])) * tts_speed * 100
+         chr_prev += len(chunk[i])
+         # Append the SubRipItem to the list of subtitles
+         subtitles.append(sub)
+         subs = pysrt.SubRipFile(subtitles)
+         subs.save("./sources/subtitles.srt", encoding='utf-8')
 
 if __name__ == "__main__": 
-    text = "Am I the Asshole for playing with my child's toys that he doesn't play with anymore?  My six year old son has an obsession with collecting toys, but he cannot keep them organized and they end up in a huge pile. I have asked him to clean up his toys many times but he doesn't, and I am not going to force him to do so.:Sometimes I find myself playing with his toys, mainly the ones that are hidden in the pile because he doesn't play with them anymore. I find it fun and it helps pass the time when I'm bored. :He sees me playing with his toys and gets mad, saying that he wanted to play with those toys and I should leave them alone. :I tell him that he hasn't played with the toys in a long time and he should organize his toys if he wanted to play with them. :Am I the asshole for playing with my son's toys?"
+    #text = "Am I the Asshole for playing with my child's toys that he doesn't play with anymore?  My six year old son has an obsession with collecting toys, but he cannot keep them organized and they end up in a huge pile. I have asked him to clean up his toys many times but he doesn't, and I am not going to force him to do so.:Sometimes I find myself playing with his toys, mainly the ones that are hidden in the pile because he doesn't play with them anymore. I find it fun and it helps pass the time when I'm bored. :He sees me playing with his toys and gets mad, saying that he wanted to play with those toys and I should leave them alone. :I tell him that he hasn't played with the toys in a long time and he should organize his toys if he wanted to play with them. :Am I the asshole for playing with my son's toys?"
+    text = "Am I the Asshole for Breaking \n Up with Him Over His\n Hot Dog Habit? Post: I met this amazing guy a few \nmonths ago. We hit it\n off right away and had\n an amazing \nfirst couple of\n dates. We started seeing\n each other more often,\n and things were really good\n, until I noticed something strange\n. :Every time I went to\n his house, he was eating a hot\n dog. Just one hot dog. Every\n single time. I couldn't believe it\n. :At first I tried not to be too judgmental\n, but it got to a point where I\n was getting really annoyed. I\n'd try to talk to him\n while he was eating,\n and he'd just be picking at\n his hot dog the whole time\n. :I was getting frustrated, so\n I eventually talked to him about it\n. He just kind of laughed and\n said it was no big deal.\n So I broke up with him. :Now\n friends are telling me I'm being an asshole. Was I out of line for ending our relationship just because he ate a hot dog every day?"
+
     print(len(text))
     print(audio.info.length)
     tts_speed = audio.info.length/len(text)
     print("speed is ", tts_speed)
     make_srt(text)
-    
     
